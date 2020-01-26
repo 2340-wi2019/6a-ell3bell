3767bb7de1dad3317fc4b6433575f353abba2747
ell3bell <58443565+ell3bell@users.noreply.github.com>
2020-01-26 14:13:56 -0600

    Add files via upload

---
+++ assignment06/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>assignment06</title>
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <!--
+            Allegra French
+            assignment06
+            INFO_2340_WW
+            Thoendel
+            1.26.2020
+        -->
+
+    <h1>Hello, world!</h1>
+    <div>Allow me to introduce myself! My name is <?php echo "Allegra French!"; ?></div>
+    <div>I am <?php echo "26"; ?> years old and I a student at Metropolitan Community College.</div>
+    <div>I am currently studying <? echo "Computer Programming"; ?> and I hope to graduate in <?php echo "2020."; ?></div>
+    
+
+</body>
+
+</html>
